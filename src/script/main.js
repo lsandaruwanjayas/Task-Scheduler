1a50be20ff302d92214bf1d71e2a2c08051b02dc
lsandaruwanjayas <fgod406@gmail.com>
2024-11-20 15:58:22 +0530

    Input output test

--- src/script/main.js
+++ src/script/main.js
@@ -13,6 +13,41 @@ const descriptionInput = document.getElementById("description");
 const taskData = [];
 let currentTask = {};
 
-openTaskFormBtn.addEventListener("click",  () => {
-    taskForm.classList.toggle("hidden")
-})
+openTaskFormBtn.addEventListener("click", () => {
+  taskForm.classList.toggle("hidden");
+});
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+});
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `<div class="task" id="${id}"></div>
+    <p><strong>Title: </strong>${title}</p>
+    <p><strong>Date: </strong>${date}</p>
+    <p><strong>Description: </strong>${description}</p>`
+  });
+});
